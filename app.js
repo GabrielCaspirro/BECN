9216af334ea3d4d8dbfb03543e6d40352b9784de
GabrielCaspirro <gabriel.gcd08@gmail.com>
2026-07-11 20:14:55 -0300

    root

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use("/api", require("./routes/dados"));
 
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
